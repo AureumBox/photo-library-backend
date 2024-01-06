a12327f67112fd1b2cb6fca686507b651b6863f9
Aurimart Garcia <aurimart.liliana@gmail.com>
2024-01-05 22:46:52 -0400

    first test

--- index.py
+++ index.py
@@ -112,7 +112,7 @@ mobilenet_model.trainable = False  # freezing
 
 num_classes = 14
 
-inputs = keras.Input(shape=(150, 150, 3))
+inputs = keras.Input(shape=(image_size[0], image_size[1], 3))
 
 x = mobilenet_model(inputs, training=False)
 
@@ -140,7 +140,7 @@ model = keras.models.load_model('models/classifier1.h5')
 
 # %%
 # Probar lmao
-img = keras.preprocessing.image.load_img("C:\Users\aurim\Desktop\sitios-fantasticos-descubrir-bolivar-fc12c3d903a6a32f6a61daf37f8fa7cf.jpg", target_size=image_size)
+img = keras.preprocessing.image.load_img("C:\\Users\\aurim\Desktop\sitios-fantasticos-descubrir-bolivar-fc12c3d903a6a32f6a61daf37f8fa7cf.jpg", target_size=image_size)
 plt.imshow(img)
 
 img_array = keras.preprocessing.image.img_to_array(img)
@@ -153,90 +153,3 @@ predicted_class = np.argmax(predictions[0])  # Get the index of the highest scor
 class_names = ["Animals", "Architecture", "Battles", "Bookcovers", "BookPages", "Foods", "Landscapes", "Maps", "Paintings", "People", "Plants", "Rivers", "Sculptures", "Stamps"]
 
 print(f"This image is most likely {class_names[predicted_class]} with a {100 * np.max(predictions[0]):.2f}% confidence.")
-
-
-"""
-    # %%
-    # Build a model
-    def make_model(input_shape, num_classes):
-        inputs = keras.Input(shape=input_shape)
-
-        # Entry block
-        x = layers.Rescaling(1.0 / 255)(inputs)
-        x = layers.Conv2D(128, 3, strides=2, padding="same")(x)
-        x = layers.BatchNormalization()(x)
-        x = layers.Activation("relu")(x)
-
-        previous_block_activation = x  # Set aside residual
-
-        for size in [256, 512, 728]:
-            x = layers.Activation("relu")(x)
-            x = layers.SeparableConv2D(size, 3, padding="same")(x)
-            x = layers.BatchNormalization()(x)
-
-            x = layers.Activation("relu")(x)
-            x = layers.SeparableConv2D(size, 3, padding="same")(x)
-            x = layers.BatchNormalization()(x)
-
-            x = layers.MaxPooling2D(3, strides=2, padding="same")(x)
-
-            # Project residual
-            residual = layers.Conv2D(size, 1, strides=2, padding="same")(
-                previous_block_activation
-            )
-            x = layers.add([x, residual])  # Add back residual
-            previous_block_activation = x  # Set aside next residual
-
-        x = layers.SeparableConv2D(1024, 3, padding="same")(x)
-        x = layers.BatchNormalization()(x)
-        x = layers.Activation("relu")(x)
-
-        x = layers.GlobalAveragePooling2D()(x)
-        if num_classes == 2:
-            units = 1
-        else:
-            units = num_classes
-
-        x = layers.Dropout(0.25)(x)
-        # We specify activation=None so as to return logits
-        outputs = layers.Dense(units, activation=None)(x)
-        return keras.Model(inputs, outputs)
-
-
-    model = make_model(input_shape=image_size + (3,), num_classes=2)
-    keras.utils.plot_model(model, show_shapes=True)
-
-    # %%
-    # Train the model
-    epochs = 25
-
-    callbacks = [
-        keras.callbacks.ModelCheckpoint("save_at_{epoch}.keras"),
-    ]
-    model.compile(
-        optimizer=keras.optimizers.Adam(3e-4),
-        loss=keras.losses.BinaryCrossentropy(from_logits=True),
-        metrics=[keras.metrics.BinaryAccuracy(name="acc")],
-    )
-    model.fit(
-        train_ds,
-        epochs=epochs,
-        callbacks=callbacks,
-        validation_data=val_ds,
-    )
-
-    # %%
-    # Run inference on new data
-    img = keras.utils.load_img("PetImages/Cat/6779.jpg", target_size=image_size)
-    plt.imshow(img)
-
-    img_array = keras.utils.img_to_array(img)
-    img_array = keras.ops.expand_dims(img_array, 0)  # Create batch axis
-
-    predictions = model.predict(img_array)
-    score = float(keras.ops.sigmoid(predictions[0][0]))
-    print(f"This image is {100 * (1 - score):.2f}% cat and {100 * score:.2f}% dog.")
-
-    #%%
-
-"""
