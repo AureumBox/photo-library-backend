75667ddfcf9abb025cd1df9628074bfd670d6f0a
Aurimart Garcia <aurimart.liliana@gmail.com>
2024-01-14 13:19:47 -0400

    receive img to classify

---
+++ app/image_classifier/model_specifications.py
@@ -0,0 +1,19 @@
+class_names = [
+    "animals",
+    "architecture",
+    "battles",
+    "bookcovers",
+    "book_pages",
+    "foods",
+    "landscapes",
+    "maps",
+    "paintings",
+    "people",
+    "plants",
+    "rivers",
+    "sculptures",
+    "stamps",
+]
+num_classes = len(class_names)
+image_size = (180, 180)
+batch_size = 20
--- app/routers/images.py
+++ app/routers/images.py
@@ -1,5 +1,12 @@
 from fastapi import APIRouter, Depends, File, UploadFile, Form
 from fastapi.responses import JSONResponse
+
+import tensorflow as tf
+import numpy as np
+import io
+from PIL import Image
+from app.image_classifier.model_specifications import image_size, class_names
+
 from app.config.database import SessionLocal
 from app.schemas import images as schemas
 from app.dependencies import get_db
@@ -22,9 +29,38 @@ def get_images(db: SessionLocal = Depends(get_db)):
     "/classify",
     description="Classify an image with the model",
 )
-def classify_image(file: UploadFile = File(...), db: SessionLocal = Depends(get_db)):
+async def classify_image(file: UploadFile = File(...), db: SessionLocal = Depends(get_db)):
+    model = tf.keras.models.load_model("app/image_classifier/models/classifier1.h5")
+
     try:
-        return {"message": "lmao World", "image": "hola"}
+        file_contents = await file.read()
+
+        # Convertir los bytes a una imagen PIL
+        img = tf.keras.preprocessing.image.load_img(io.BytesIO(file_contents), target_size=image_size)
+
+        # Convertir la imagen PIL a un numpy array
+        img_array = tf.keras.preprocessing.image.img_to_array(img)
+
+        # Añadir una dimensión extra al principio del array
+        img_array = np.expand_dims(img_array, axis=0)
+
+        # Normalizar los pixeles a valores entre 0 y 1
+        img_array /= 255.
+
+        # Pasar la imagen por el modelo
+        predictions = model.predict(img_array)
+        predicted_class = np.argmax(predictions[0])
+
+        print(predictions)
+        print(predicted_class)
+
+        return JSONResponse(
+            status_code=200,
+            content={
+                "message": "Imagen clasificada con éxito",
+                "category": f"{class_names[predicted_class]}",
+            },
+        )
     except Exception as e:
         return JSONResponse(
             status_code=500,
