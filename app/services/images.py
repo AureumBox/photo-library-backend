9a72a48408129ffa2947d020f5861698f539a787
Aurimart Garcia <aurimart.liliana@gmail.com>
2024-01-17 21:57:57 -0400

    added details in image by id

--- app/services/images.py
+++ app/services/images.py
@@ -7,11 +7,52 @@ from enum import Enum
 from typing import Optional
 
 from app.models.images import Image as Model
+from app.models.works import Work
+from app.models.publications import Publication
+from app.models.authors import Author
+
 from app.schemas import images as schemas
 
 
+def get_image2(db: Session, id: str):
+    image = db.query(Model).filter(Model.id == id).first()
+    work = db.query(Work).filter(Work.id == id).first()
+    publication = db.query(Publication).filter(Publication.id == id).first()
+    author = db.query(Author).filter(Author.id == id).first()
+    return image
+
+
 def get_image(db: Session, id: str):
-    return db.query(Model).filter(Model.id == id).first()
+    image = db.query(Model).filter(Model.id == id).first()
+    if not image:
+        return None
+    image_dict = image.__dict__
+    # Get the work, publication, and author details based on their IDs
+    work = (
+        db.query(Work).filter(Work.id == image.work_id).first()
+        if image.work_id
+        else None
+    )
+    publication = (
+        db.query(Publication).filter(Publication.id == image.publication_id).first()
+        if image.publication_id
+        else None
+    )
+    author = (
+        db.query(Author).filter(Author.id == image.author_id).first()
+        if image.author_id
+        else None
+    )
+
+    image_dict["work"] = work.__dict__ if work else None
+    image_dict["publication"] = publication.__dict__ if publication else None
+    image_dict["author"] = author.__dict__ if author else None
+
+    image_dict.pop("work_id", None)
+    image_dict.pop("publication_id", None)
+    image_dict.pop("author_id", None)
+
+    return {"image": image_dict}
 
 
 def get_images_by_tag(db: Session, tag: str):
@@ -66,18 +107,18 @@ def save_image(
     with Path(image_path).open("wb") as buffer:
         shutil.copyfileobj(file.file, buffer)
 
-
+    source = f"/images/{id}/file"
     image = schemas.ImageCreate.model_construct(
-        id = id,
-        work_id = work_id,
-        author_id = author_id,
-        publication_id = publication_id,
-        type = type,
-        description = description,
-        tag = tag,
-        copyright = copyright,
-        reference = reference,
-        source = image_path,  
+        id=id,
+        work_id=work_id,
+        author_id=author_id,
+        publication_id=publication_id,
+        type=type,
+        description=description,
+        tag=tag,
+        copyright=copyright,
+        reference=reference,
+        source=image_path,
     )
 
     db_image = create_image(db, image)
